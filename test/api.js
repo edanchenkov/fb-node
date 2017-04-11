01b4c5c2aff51e9adaf7e51765f54d4a02d9c707
Egor Danchenkov <icepnyx@gmail.com>
2017-04-11 12:36:28 +0300

    Update tests

--- test/api.js
+++ test/api.js
@@ -4,56 +4,63 @@ var should = require('should');
 var api = require('./../lib/api');
 
 var endpoint = 'https://graph.facebook.com';
-var token = 'ENTER VALID TOKEN';
+var token = '';
 
 var post = null;
 
-describe('Library for Facebook Graph API v2.*', function(){
+describe('Library for Facebook Graph API v2.*', function () {
 
-  it('should set api token', function(done){
-    api.token = 'this token';
-    api.token.should.equal('this token');
-    done();
-  });
 
-  it('should set api endpoint', function(done){
-    api.endpoint = 'this endpoint';
-    api.endpoint.should.equal('this endpoint');
-    done();
-  });
-  
-  it('should recieved status code 200, when GET /me', function(done){
-    api.endpoint = endpoint;
-    api.token = token;
-    var url = '/me';
-    api.get(url).then(function (data) {
-      data.response.statusCode.should.be.equal(200);
-    }).finally(function(){
-      done();
+    if(!token) {
+        console.error(new Error('No token provided, check set access token in test/api.js'));
+        return;
+    }
+
+    it('should set api token', function (done) {
+        api.token = 'this token';
+        api.token.should.equal('this token');
+        done();
     });
-  });
 
-  it('should recieved status code 200, when POST /me/feed', function(done){
-    this.timeout(5000);
-    var url = '/me/feed';
-    var params = { message: 'Test message' };   
-    api.post(url, params).then(function (data) {
-      data.json.should.have.property('id');
-      post = data.json.id;
-      data.response.statusCode.should.be.equal(200);
-    }).finally(function(){
-      done();
+    it('should set api endpoint', function (done) {
+        api.endpoint = 'this endpoint';
+        api.endpoint.should.equal('this endpoint');
+        done();
     });
-  });
 
-  it('should recieved status code 200, when DELETE /{post-id}', function(done){
-    this.timeout(5000);
-    var url = '/' + post;
-    api.delete(url).then(function (data) {
-      data.response.statusCode.should.be.equal(200);
-    }).finally(function(){
-      done();
+    it('should received status code 200, when GET /me', function (done) {
+        api.endpoint = endpoint;
+        api.token = token;
+        var url = '/me';
+        api.get(url).then(function (data) {
+            console.log('data')
+            data.response.statusCode.should.be.equal(200);
+        }).finally(function () {
+            done();
+        });
     });
-  });
+
+    it('should received status code 200, when POST /me/feed', function (done) {
+        this.timeout(5000);
+        var url = '/me/feed';
+        var params = { message : 'Test message' };
+        api.post(url, params).then(function (data) {
+            data.json.should.have.property('id');
+            post = data.json.id;
+            data.response.statusCode.should.be.equal(200);
+        }).finally(function () {
+            done();
+        });
+    });
+
+    it('should received status code 200, when DELETE /{post-id}', function (done) {
+        this.timeout(5000);
+        var url = '/' + post;
+        api.delete(url).then(function (data) {
+            data.response.statusCode.should.be.equal(200);
+        }).finally(function () {
+            done();
+        });
+    });
 
 });
